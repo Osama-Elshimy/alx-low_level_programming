a5b916b976f5836d1e02ccc8b95d21f8e0bf2fe6
Osama-Elshimy <Osama.Elshimy.o.e@gmail.com>
2023-10-05 15:27:03 +0300

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints a message with proper grammar and new line
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
+
