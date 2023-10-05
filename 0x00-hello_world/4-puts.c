fe1b2efd8eea18787550e11e3ca7bfa8efc139ed
Osama-Elshimy <Osama.Elshimy.o.e@gmail.com>
2023-10-05 15:22:45 +0300

    add bitty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
-int main() {
-    puts("Programming is like building a multilingual puzzle");
-    return 0;
+/**
+ * main - Entry point
+ *
+ * Description: Prints a message followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
 }
-
