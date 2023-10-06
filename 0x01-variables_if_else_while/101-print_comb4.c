088d2e0f4e115df50fd2b427de9384fc97421cc1
Osama-Elshimy <Osama.Elshimy.o.e@gmail.com>
2023-10-06 10:52:34 +0300

    fix 101-print-comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -3,36 +3,26 @@
 /**
  * main - Entry point of the program
  *
- * Description: Prints all possible combinations of three digits
- * Numbers must be separated by `,` and followed by a space
- * The three digits must be different
- * 01 and 10 are considered the same combination of the two digits 0 and 1
- * Print only the smallest combination of two digits
- * Numbers should be printed in ascending order, with three digits
- * using the putchar function.
- * not allowed to use char variables
+ * Description: Prints all possible different combinations of three digits,
+ * separated by ", " and in ascending order, using putchar function.
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-
-int digit1, digit2, digit3;
-digit1 = 0;
-
+int digit1 = 0;
 while (digit1 <= 7)
-digit2 = digit1 + 1;
 {
-
+int digit2 = digit1 + 1;
 while (digit2 <= 8)
 {
-digit3 = digit2 + 1;
+int digit3 = digit2 + 1;
 while (digit3 <= 9)
 {
 putchar(digit1 + '0');
 putchar(digit2 + '0');
 putchar(digit3 + '0');
 
-if (digit1 != 7 || digit2 != 8 || digit3 != 9)
+if (!(digit1 == 7 && digit2 == 8 && digit3 == 9))
 {
 putchar(',');
 putchar(' ');
@@ -40,10 +30,8 @@ putchar(' ');
 
 digit3++;
 }
-
 digit2++;
 }
-
 digit1++;
 }
 
