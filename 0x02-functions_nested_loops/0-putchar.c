99c0504860280cf80c74995401d4b6616d0b7b37
Osama-Elshimy <Osama.Elshimy.o.e@gmail.com>
2023-10-10 08:48:19 +0300

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "_putchar.c"
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Print "_putchar"
+ * Return always 0 (Success)
+ * Not allowed to use the standard library
+ */
+
+int main(void)
+{
+  _putchar('_');
+  _putchar('p');
+  _putchar('u');
+  _putchar('t');
+  _putchar('c');
+  _putchar('h');
+  _putchar('a');
+  _putchar('r');
+  _putchar('\n');
+  return (0);
+}
