7c4aa177d607e9ec5a78bc0b575ea662d3845e39
Osama-Elshimy <Osama.Elshimy.o.e@gmail.com>
2023-10-10 23:50:45 +0300

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,53 +8,53 @@
 
 void print_times_table(int n)
 {
-    int i, j, result;
+int i, j, result;
 
-    if (n < 0 || n > 15)
-    {
-        return;
-    }
+if (n < 0 || n > 15)
+{
+return;
+}
 
-    for (i = 0; i <= n; i++)
-    {
-        for (j = 0; j <= n; j++)
-        {
-            result = i * j;
+for (i = 0; i <= n; i++)
+{
+for (j = 0; j <= n; j++)
+{
+result = i * j;
 
-            if (j > 0)
-            {
-                _putchar(',');
-                _putchar(' ');
+if (j > 0)
+{
+_putchar(',');
+_putchar(' ');
 
-                if (result < 10)
-                {
-                    _putchar(' ');
-                    _putchar(' ');
-                }
-                else if (result < 100)
-                {
-                    _putchar(' ');
-                }
-            }
+if (result < 10)
+{
+_putchar(' ');
+_putchar(' ');
+}
+else if (result < 100)
+{
+_putchar(' ');
+}
+}
 
-            if (result < 10)
-            {
-                _putchar(result + '0');
-            }
-            else if (result < 100)
-            {
-                _putchar(result / 10 + '0');
-                _putchar(result % 10 + '0');
-            }
-             else
-            {
-                _putchar(result / 100 + '0');
-                _putchar((result / 10 % 10) + '0');
-                _putchar(result % 10 + '0');
-            }
+if (result < 10)
+{
+_putchar(result + '0');
+}
+else if (result < 100)
+{
+_putchar(result / 10 + '0');
+_putchar(result % 10 + '0');
+}
+else
+{
+_putchar(result / 100 + '0');
+_putchar((result / 10 % 10) + '0');
+_putchar(result % 10 + '0');
+}
 
-        }
+}
 
-        _putchar('\n');
-    }
+_putchar('\n');
+}
 }
