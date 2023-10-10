5c7709bb480dad94853dd00b1a276df8057375e5
Osama-Elshimy <Osama.Elshimy.o.e@gmail.com>
2023-10-10 14:33:24 +0300

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Print alphabet in lowercase followed by a new line
+ * Not allowed to use standard library
+ * Use only _putchar
+ * Use _putchar only twice in code
+ *
+ * Return: Always void.
+ */
+
+void print_alphabet(void)
+{
+int i; 
+for (i = 97; i <= 122; i++)
+{
+_putchar(i);
+}
+
+_putchar('\n');
+}
