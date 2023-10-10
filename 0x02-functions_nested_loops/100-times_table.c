64b37ff22665a79729d323b91530ec7c7b7736c6
Osama-Elshimy <Osama.Elshimy.o.e@gmail.com>
2023-10-10 23:55:16 +0300

    fix style

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,36 +11,27 @@ void print_times_table(int n)
 int i, j, result;
 
 if (n < 0 || n > 15)
-{
 return;
-}
-
 for (i = 0; i <= n; i++)
 {
 for (j = 0; j <= n; j++)
 {
 result = i * j;
-
 if (j > 0)
 {
 _putchar(',');
 _putchar(' ');
-
 if (result < 10)
 {
 _putchar(' ');
 _putchar(' ');
 }
 else if (result < 100)
-{
 _putchar(' ');
-}
 }
 
 if (result < 10)
-{
 _putchar(result + '0');
-}
 else if (result < 100)
 {
 _putchar(result / 10 + '0');
@@ -52,9 +43,7 @@ _putchar(result / 100 + '0');
 _putchar((result / 10 % 10) + '0');
 _putchar(result % 10 + '0');
 }
-
 }
-
 _putchar('\n');
 }
 }
