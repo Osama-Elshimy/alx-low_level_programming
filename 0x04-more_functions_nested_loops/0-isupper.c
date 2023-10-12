b351ab71574fe280e67e883ed6b51f33ee7b0a69
Osama-Elshimy <Osama.Elshimy.o.e@gmail.com>
2023-10-12 06:47:25 +0300

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * _isupper - Check if a letter is uppercase
+ * @c:  Letter to be checked
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+
+int _isupper(int c)
+{
+  if (c >= 65 && c <= 90)
+  {
+    return (1);
+  }
+  return (0);
+}
