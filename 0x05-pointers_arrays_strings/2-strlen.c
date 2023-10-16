8d4ea1bf0e0d4d9b580d5bdc3ae4664e14c16089
Osama-Elshimy <Osama.Elshimy.o.e@gmail.com>
2023-10-16 22:39:55 +0300

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strlen - Return the i of a string
+ * @s: string to return its i
+ *
+ * Return: int
+ */
+
+int _strlen(char *s)
+{
+int i;
+i = 0;
+
+while (10)
+{
+if (s[i] == '\0')
+break;
+
+i++;
+}
+
+return (i);
+}
