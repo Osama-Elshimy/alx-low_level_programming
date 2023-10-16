d3c9d6ae1cd8ff9c0dde6f35c9dfeaa4d9e9d054
Osama-Elshimy <Osama.Elshimy.o.e@gmail.com>
2023-10-17 01:03:59 +0300

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -13,17 +13,16 @@
 
 char *_strcpy(char *dest, char *src)
 {
-int length, i;
-length = 0;
+int i;
+i = 0;
 
-while (src[length] != '\0')
-length++;
-
-while (i < length)
+while (src[i] != '\0')
 {
 dest[i] = src[i];
 i++;
 }
+
+dest[i] = '\0';
 
 return (dest);
 }
