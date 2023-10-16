ad3b1452fed402660abc1e9b521dcc4ee9a33587
Osama-Elshimy <Osama.Elshimy.o.e@gmail.com>
2023-10-17 01:45:51 +0300

    fix

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -2,36 +2,60 @@
 #include <stdlib.h>
 #include <time.h>
 
+static unsigned long next = 1;
+
 /**
- * generate_password - Generates a random valid password for 101-crackme
- * Return: A randomly generated password
+ * _rand - custom rand function that generates random numbers from 33 - 122
+ *
+ * Return: random number
  */
-char *generate_password(void)
+int _rand(void)
 {
-static char password[11];
-const char charset[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-int i;
+	unsigned int value;
 
-srand(time(0));
-
-for (i = 0; i < 10; i++)
-{
-password[i] = charset[rand() % (sizeof(charset) - 1)];
-}
-
-password[10] = '\0';
-
-return (password);
+	next = next * 1103515245 + 12345;
+	value = (unsigned int)(next / 65536) % 123;
+	if (value >= 33)
+		return (value);
+	return (_rand());
 }
 
 /**
-  * main - entry function
-  * Return: Always 0
-  */
+ * _srand - custom implementation of srand
+ *
+ * @seed: Seed to generate random number
+ */
+void _srand(unsigned int seed)
+{
+	next = seed;
+}
+
+/**
+ * main - generates a key whose ASCII value adds up to 0xAD4
+ *
+ * Return: EXIT_SUCCESS
+ */
 int main(void)
 {
-char *password = generate_password();
-printf("Generated Password: %s\n", password);
-return (0);
-}
+	int ascii_total, i, diff;
+	int r;
+	time_t seed = time(NULL);
 
+	_srand(seed);
+	ascii_total = i = 0;
+	/* 
+	 * ensure that random numbers generated don't exceed a total
+	 * of 2772
+	 */
+	while (ascii_total <= 0xAD4 - 0x7A)
+	{
+		r = (int)_rand();
+		printf("%c", r);
+		ascii_total += r;
+		i++;
+	}
+	/* get the difference left and generate remaining characters */
+	diff = (0xAD4 - ascii_total);
+	printf("%c", diff);
+	exit(EXIT_SUCCESS);
+}
