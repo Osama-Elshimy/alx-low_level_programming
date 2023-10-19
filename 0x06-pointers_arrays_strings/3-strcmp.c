b9996e9b397d11c9198f1d89b1a2109172de5bfc
Osama-Elshimy <Osama.Elshimy.o.e@gmail.com>
2023-10-19 16:45:32 +0300

    fix

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,28 +5,21 @@
  * @s2: second string
  *
  * Return: 0 if strings are equal,
- * -15 if s1 is less than s2, 15 if s1 is greater than s2
+ * Negative value if s1 is less than s2,
+ * Positive value if s1 is greater than s2
  */
 
 int _strcmp(char *s1, char *s2)
 {
-int s1_length, s2_length;
-
-s1_length = 0;
-s2_length = 0;
-
-while (s1[s1_length] != '\0')
-s1_length++;
-
-while (s2[s2_length] != '\0')
-s2_length++;
-
-if (s1_length > s2_length)
-return (15);
-else if (s2_length > s1_length)
-return (-15);
-
-else
-return (0);
+while (*s1 != '\0' && *s2 != '\0')
+{
+if (*s1 != *s2)
+{
+return (*s1 - *s2);
+}
+s1++;
+s2++;
 }
 
+return (*s1 - *s2);
+}
