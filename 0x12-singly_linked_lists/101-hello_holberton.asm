5fc506b6cd6177a5a35c7d5a5c660544542fad30
Osama-Elshimy <Osama.Elshimy.o.e@gmail.com>
2023-11-16 23:31:53 +0200

    fix 101-hello_holberton.asm #2

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,51 +1,17 @@
 section .data
-    hello db 'Hello, Holberton',0
+msg     db "Hello, Holberton", 10, 0
 
 section .text
-    global _start
+global  main
+extern  printf
 
-_start:
-    ; Call printf
-    mov rdi, hello    ; format string
-    mov rax, 0        ; syscall number for sys_write
-    call printf
+main:
+	mov  rdi, msg
+	mov  rsi, 0
+	mov  eax, 0
+	call printf
 
-    ; Exit the program
-    mov rax, 60       ; syscall number for sys_exit
-    xor rdi, rdi      ; status 0
-    syscall
-
-section .text
-global printf
-extern putchar
-
-printf:
-    ; printf(char* format, ...)
-    ; rdi: format string, ... (variadic arguments in registers)
-    ; Uses syscall number 1 for sys_write to stdout
-
-    xor rax, rax         ; Clear RAX register
-.loop:
-    ; Load the current character from the format string
-    mov al, byte [rdi]
-
-    ; Check if we've reached the null terminator (end of the string)
-    cmp al, 0
-    je .done
-
-    ; Print the character
-    mov rdi, 1            ; File descriptor 1 is stdout
-    mov rsi, rsp          ; Load the address of the character into RSI
-    mov rdx, 1            ; Print one character
-    call putchar
-
-    ; Move to the next character in the format string
-    inc rdi
-
-    ; Continue the loop
-    jmp .loop
-
-.done:
-    ret
-
+	mov eax, 0
+	xor rdi, rdi
+	syscall
 
